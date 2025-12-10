dd544031fff475449a5122dcdabc1d6c33f6f677
Ashleymhar Bautista <ashleymharbautista@gmail.com>
2025-12-10 12:47:41 +0800

    added connect, update, load/read and delete; programmed.

--- Licayo_FinalProject/Form1.Designer.vb
+++ Licayo_FinalProject/Form1.Designer.vb
@@ -32,160 +32,210 @@ Partial Class Form1
         Me.lblPosition = New System.Windows.Forms.Label()
         Me.lblSalary = New System.Windows.Forms.Label()
         Me.lblDepartment = New System.Windows.Forms.Label()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.dgvEmployees = New System.Windows.Forms.DataGridView()
         Me.btnAdd = New System.Windows.Forms.Button()
         Me.btnUpdate = New System.Windows.Forms.Button()
         Me.btnDelete = New System.Windows.Forms.Button()
-        Me.btnRead = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.lblSearch = New System.Windows.Forms.Label()
         Me.btnConnect = New System.Windows.Forms.Button()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvEmployees, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'txtId
         '
-        Me.txtId.Location = New System.Drawing.Point(98, 42)
+        Me.txtId.Location = New System.Drawing.Point(131, 52)
+        Me.txtId.Margin = New System.Windows.Forms.Padding(4)
         Me.txtId.Name = "txtId"
-        Me.txtId.Size = New System.Drawing.Size(100, 20)
+        Me.txtId.Size = New System.Drawing.Size(132, 22)
         Me.txtId.TabIndex = 0
         '
         'txtDepartment
         '
-        Me.txtDepartment.Location = New System.Drawing.Point(98, 198)
+        Me.txtDepartment.Location = New System.Drawing.Point(131, 244)
+        Me.txtDepartment.Margin = New System.Windows.Forms.Padding(4)
         Me.txtDepartment.Name = "txtDepartment"
-        Me.txtDepartment.Size = New System.Drawing.Size(100, 20)
+        Me.txtDepartment.Size = New System.Drawing.Size(132, 22)
         Me.txtDepartment.TabIndex = 1
         '
         'txtSalary
         '
-        Me.txtSalary.Location = New System.Drawing.Point(98, 159)
+        Me.txtSalary.Location = New System.Drawing.Point(131, 196)
+        Me.txtSalary.Margin = New System.Windows.Forms.Padding(4)
         Me.txtSalary.Name = "txtSalary"
-        Me.txtSalary.Size = New System.Drawing.Size(100, 20)
+        Me.txtSalary.Size = New System.Drawing.Size(132, 22)
         Me.txtSalary.TabIndex = 2
         '
         'txtPosition
         '
-        Me.txtPosition.Location = New System.Drawing.Point(98, 120)
+        Me.txtPosition.Location = New System.Drawing.Point(131, 148)
+        Me.txtPosition.Margin = New System.Windows.Forms.Padding(4)
         Me.txtPosition.Name = "txtPosition"
-        Me.txtPosition.Size = New System.Drawing.Size(100, 20)
+        Me.txtPosition.Size = New System.Drawing.Size(132, 22)
         Me.txtPosition.TabIndex = 3
         '
         'txtName
         '
-        Me.txtName.Location = New System.Drawing.Point(98, 81)
+        Me.txtName.Location = New System.Drawing.Point(131, 100)
+        Me.txtName.Margin = New System.Windows.Forms.Padding(4)
         Me.txtName.Name = "txtName"
-        Me.txtName.Size = New System.Drawing.Size(100, 20)
+        Me.txtName.Size = New System.Drawing.Size(132, 22)
         Me.txtName.TabIndex = 4
         '
         'lblId
         '
         Me.lblId.AutoSize = True
-        Me.lblId.Location = New System.Drawing.Point(95, 26)
+        Me.lblId.Location = New System.Drawing.Point(127, 32)
+        Me.lblId.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblId.Name = "lblId"
-        Me.lblId.Size = New System.Drawing.Size(16, 13)
+        Me.lblId.Size = New System.Drawing.Size(18, 16)
         Me.lblId.TabIndex = 5
         Me.lblId.Text = "Id"
         '
         'lblName
         '
         Me.lblName.AutoSize = True
-        Me.lblName.Location = New System.Drawing.Point(95, 65)
+        Me.lblName.Location = New System.Drawing.Point(127, 80)
+        Me.lblName.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblName.Name = "lblName"
-        Me.lblName.Size = New System.Drawing.Size(35, 13)
+        Me.lblName.Size = New System.Drawing.Size(44, 16)
         Me.lblName.TabIndex = 6
         Me.lblName.Text = "Name"
         '
         'lblPosition
         '
         Me.lblPosition.AutoSize = True
-        Me.lblPosition.Location = New System.Drawing.Point(95, 104)
+        Me.lblPosition.Location = New System.Drawing.Point(127, 128)
+        Me.lblPosition.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblPosition.Name = "lblPosition"
-        Me.lblPosition.Size = New System.Drawing.Size(44, 13)
+        Me.lblPosition.Size = New System.Drawing.Size(55, 16)
         Me.lblPosition.TabIndex = 7
         Me.lblPosition.Text = "Position"
         '
         'lblSalary
         '
         Me.lblSalary.AutoSize = True
-        Me.lblSalary.Location = New System.Drawing.Point(95, 143)
+        Me.lblSalary.Location = New System.Drawing.Point(127, 176)
+        Me.lblSalary.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblSalary.Name = "lblSalary"
-        Me.lblSalary.Size = New System.Drawing.Size(36, 13)
+        Me.lblSalary.Size = New System.Drawing.Size(46, 16)
         Me.lblSalary.TabIndex = 8
         Me.lblSalary.Text = "Salary"
         '
         'lblDepartment
         '
         Me.lblDepartment.AutoSize = True
-        Me.lblDepartment.Location = New System.Drawing.Point(95, 182)
+        Me.lblDepartment.Location = New System.Drawing.Point(127, 224)
+        Me.lblDepartment.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblDepartment.Name = "lblDepartment"
-        Me.lblDepartment.Size = New System.Drawing.Size(62, 13)
+        Me.lblDepartment.Size = New System.Drawing.Size(77, 16)
         Me.lblDepartment.TabIndex = 9
         Me.lblDepartment.Text = "Department"
         '
-        'DataGridView1
+        'dgvEmployees
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(79, 273)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(357, 157)
-        Me.DataGridView1.TabIndex = 10
+        Me.dgvEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvEmployees.Location = New System.Drawing.Point(307, 52)
+        Me.dgvEmployees.Margin = New System.Windows.Forms.Padding(4)
+        Me.dgvEmployees.Name = "dgvEmployees"
+        Me.dgvEmployees.RowHeadersWidth = 51
+        Me.dgvEmployees.Size = New System.Drawing.Size(476, 193)
+        Me.dgvEmployees.TabIndex = 10
         '
         'btnAdd
         '
-        Me.btnAdd.Location = New System.Drawing.Point(361, 244)
+        Me.btnAdd.Location = New System.Drawing.Point(339, 253)
+        Me.btnAdd.Margin = New System.Windows.Forms.Padding(4)
         Me.btnAdd.Name = "btnAdd"
-        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.Size = New System.Drawing.Size(100, 28)
         Me.btnAdd.TabIndex = 11
         Me.btnAdd.Text = "Add"
         Me.btnAdd.UseVisualStyleBackColor = True
         '
         'btnUpdate
         '
-        Me.btnUpdate.Location = New System.Drawing.Point(82, 436)
+        Me.btnUpdate.Location = New System.Drawing.Point(367, 302)
+        Me.btnUpdate.Margin = New System.Windows.Forms.Padding(4)
         Me.btnUpdate.Name = "btnUpdate"
-        Me.btnUpdate.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpdate.Size = New System.Drawing.Size(100, 28)
         Me.btnUpdate.TabIndex = 12
         Me.btnUpdate.Text = "Update"
         Me.btnUpdate.UseVisualStyleBackColor = True
         '
         'btnDelete
         '
-        Me.btnDelete.Location = New System.Drawing.Point(361, 436)
+        Me.btnDelete.Location = New System.Drawing.Point(496, 302)
+        Me.btnDelete.Margin = New System.Windows.Forms.Padding(4)
         Me.btnDelete.Name = "btnDelete"
-        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.Size = New System.Drawing.Size(100, 28)
         Me.btnDelete.TabIndex = 13
         Me.btnDelete.Text = "Delete"
         Me.btnDelete.UseVisualStyleBackColor = True
         '
-        'btnRead
+        'btnClear
         '
-        Me.btnRead.Location = New System.Drawing.Point(98, 244)
-        Me.btnRead.Name = "btnRead"
-        Me.btnRead.Size = New System.Drawing.Size(75, 23)
-        Me.btnRead.TabIndex = 15
-        Me.btnRead.Text = "Read"
-        Me.btnRead.UseVisualStyleBackColor = True
+        Me.btnClear.Location = New System.Drawing.Point(554, 253)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(99, 33)
+        Me.btnClear.TabIndex = 17
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Location = New System.Drawing.Point(447, 253)
+        Me.btnLoad.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(100, 28)
+        Me.btnLoad.TabIndex = 18
+        Me.btnLoad.Text = "Load"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(130, 302)
+        Me.txtSearch.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(133, 22)
+        Me.txtSearch.TabIndex = 20
+        '
+        'lblSearch
+        '
+        Me.lblSearch.AutoSize = True
+        Me.lblSearch.Location = New System.Drawing.Point(135, 282)
+        Me.lblSearch.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblSearch.Name = "lblSearch"
+        Me.lblSearch.Size = New System.Drawing.Size(50, 16)
+        Me.lblSearch.TabIndex = 19
+        Me.lblSearch.Text = "Search"
         '
         'btnConnect
         '
-        Me.btnConnect.Location = New System.Drawing.Point(337, 117)
+        Me.btnConnect.Location = New System.Drawing.Point(130, 347)
+        Me.btnConnect.Margin = New System.Windows.Forms.Padding(4)
         Me.btnConnect.Name = "btnConnect"
-        Me.btnConnect.Size = New System.Drawing.Size(75, 23)
-        Me.btnConnect.TabIndex = 16
+        Me.btnConnect.Size = New System.Drawing.Size(133, 67)
+        Me.btnConnect.TabIndex = 21
         Me.btnConnect.Text = "Connect"
         Me.btnConnect.UseVisualStyleBackColor = True
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.ClientSize = New System.Drawing.Size(800, 567)
+        Me.ClientSize = New System.Drawing.Size(927, 459)
         Me.Controls.Add(Me.btnConnect)
-        Me.Controls.Add(Me.btnRead)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.lblSearch)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.btnClear)
         Me.Controls.Add(Me.btnDelete)
         Me.Controls.Add(Me.btnUpdate)
         Me.Controls.Add(Me.btnAdd)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.dgvEmployees)
         Me.Controls.Add(Me.lblDepartment)
         Me.Controls.Add(Me.lblSalary)
         Me.Controls.Add(Me.lblPosition)
@@ -196,9 +246,10 @@ Partial Class Form1
         Me.Controls.Add(Me.txtSalary)
         Me.Controls.Add(Me.txtDepartment)
         Me.Controls.Add(Me.txtId)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvEmployees, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -214,10 +265,13 @@ Partial Class Form1
     Friend WithEvents lblPosition As Label
     Friend WithEvents lblSalary As Label
     Friend WithEvents lblDepartment As Label
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents dgvEmployees As DataGridView
     Friend WithEvents btnAdd As Button
     Friend WithEvents btnUpdate As Button
     Friend WithEvents btnDelete As Button
-    Friend WithEvents btnRead As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents lblSearch As Label
     Friend WithEvents btnConnect As Button
 End Class
--- Licayo_FinalProject/Form1.vb
+++ Licayo_FinalProject/Form1.vb
@@ -4,13 +4,12 @@ Public Class Form1
 
     Dim conn As MySqlConnection
     Dim COMMAND As MySqlCommand
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
+    Private selectedId As Integer = -1
 
     Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+
         conn = New MySqlConnection
-        conn.ConnectionString = "server=localhost; userid=root; password=root; database=employee_record_system;"
+        conn.ConnectionString = "server=localhost; userid=root; password=admin; database=employee_db;"
 
         Try
 
@@ -23,22 +22,27 @@ Public Class Form1
             conn.Close()
 
         End Try
+
     End Sub
 
-    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+    Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
 
-        Dim query As String = "INSERT INTO `employee_record_system`.`employee_record_system` (`id`, `name`, `position`,`salary`, `department` ) VALUES (@id, @name, @position, @salary, @department);"
+        Dim query As String = "INSERT INTO `employee_db`.`employees` (`id`, `name`, `position`, `salary`, `department`) VALUES (@id, @name, @position, @salary, @department);"
         Try
-            Using conn As New MySqlConnection("server = localhost; userid=root; password=root; database=crud_demo.db;")
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=admin; database=employee_db;")
+
                 conn.Open()
                 Using cmd As New MySqlCommand(query, conn)
+
                     cmd.Parameters.AddWithValue("@id", CInt(txtId.Text))
-                    cmd.Parameters.AddWithValue("@name", (txtName.Text))
-                    cmd.Parameters.AddWithValue("@position", (txtPosition.Text))
-                    cmd.Parameters.AddWithValue("@salary", CInt(txtSalary.Text))
-                    cmd.Parameters.AddWithValue("@department", (txtDepartment.Text))
+                    cmd.Parameters.AddWithValue("@name", txtName.Text)
+                    cmd.Parameters.AddWithValue("@position", txtPosition.Text)
+                    cmd.Parameters.AddWithValue("@salary", Decimal.Parse(txtSalary.Text))
+                    cmd.Parameters.AddWithValue("@department", txtDepartment.Text)
+
                     cmd.ExecuteNonQuery()
-                    MessageBox.Show("Record insert successful")
+
+                    MessageBox.Show("Record insert successful!")
 
                     txtId.Clear()
                     txtName.Clear()
@@ -47,49 +51,123 @@ Public Class Form1
                     txtDepartment.Clear()
 
                 End Using
+
             End Using
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
 
     End Sub
-    Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
-        Dim query As String = "SELECT * FROM `employee_record_system`employee_record_system"
+
+    Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+
+        Dim query As String = "SELECT id, name, position, salary, department FROM employee_db.employees WHERE is_deleted = 0"
+
         Try
-            Using conn As New MySqlConnection("server = localhost; userid=root; password=root; database=employee_record_system;")
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=admin; database=employee_db;")
+
                 Dim adapter As New MySqlDataAdapter(query, conn)
                 Dim table As New DataTable()
-                adapter.Fill(table)
 
-                DataGridView1.DataSource = table
-                DataGridView1.Columns("id").Visible = False
+                adapter.Fill(table)
+                dgvEmployees.DataSource = table
+
+                dgvEmployees.Columns("id").Visible = False
 
             End Using
-
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvEmployees.CellContentDoubleClick
+
+        If e.RowIndex >= 0 Then
+
+            Dim selectedRow As DataGridViewRow = dgvEmployees.Rows(e.RowIndex)
+
+            selectedId = Convert.ToInt32(selectedRow.Cells("id").Value)
+            txtId.Text = selectedRow.Cells("id").Value.ToString()
+            txtName.Text = selectedRow.Cells("name").Value.ToString()
+            txtPosition.Text = selectedRow.Cells("position").Value.ToString()
+            txtSalary.Text = selectedRow.Cells("salary").Value.ToString()
+            txtDepartment.Text = selectedRow.Cells("department").Value.ToString()
+
+        End If
+
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+
+        If selectedId = -1 Then
+            MessageBox.Show("Select a record first.")
+            Return
+        End If
+
+        If MessageBox.Show("Are you sure you want to delete this record?", "Confirm", MessageBoxButtons.YesNo) = DialogResult.No Then
+            Return
+        End If
+
+        Dim query As String = "UPDATE `employee_db`.`employees` SET is_deleted = 1 WHERE (`id` = @id);"
+
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=admin; database=employee_db;")
+
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+
+                    cmd.Parameters.AddWithValue("@id", selectedId)
+
+                    cmd.ExecuteNonQuery()
+
+                    MessageBox.Show("Record deleted successfully!")
+
+                    txtId.Clear()
+                    txtName.Clear()
+                    txtPosition.Clear()
+                    txtSalary.Clear()
+                    txtDepartment.Clear()
+                    selectedId = -1
+
+                End Using
+
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
     End Sub
 
     Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
 
-        Dim query As String = "INSERT INTO `employee_record_system`.`employee_record_system` (`id`, `name`, `position`,`salary`, `department` ) VALUES (@id, @name, @position, @salary, @department);"
+        If selectedId = -1 Then
+            MessageBox.Show("Select a record first.")
+            Return
+        End If
 
+        Dim query As String = "UPDATE `employee_db`.`employees` 
+                                SET `id` = @id,
+                                    `name` = @name,
+                                    `position` = @position,
+                                    `salary` = @salary,
+                                    `department` = @department 
+                                     WHERE (`id` = @id);"
 
         Try
-
-            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=employee_record_system;")
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=admin; database=employee_db;")
 
                 conn.Open()
-
                 Using cmd As New MySqlCommand(query, conn)
 
                     cmd.Parameters.AddWithValue("@id", CInt(txtId.Text))
-                    cmd.Parameters.AddWithValue("@name", (txtName.Text))
-                    cmd.Parameters.AddWithValue("@position", (txtPosition.Text))
-                    cmd.Parameters.AddWithValue("@salary", CInt(txtSalary.Text))
-                    cmd.Parameters.AddWithValue("@department", (txtDepartment.Text))
+                    cmd.Parameters.AddWithValue("@name", txtName.Text)
+                    cmd.Parameters.AddWithValue("@position", txtPosition.Text)
+                    cmd.Parameters.AddWithValue("@salary", Decimal.Parse(txtSalary.Text))
+                    cmd.Parameters.AddWithValue("@department", txtDepartment.Text)
+
                     cmd.ExecuteNonQuery()
+
                     MessageBox.Show("Record updated successfully!")
 
                     txtId.Clear()
@@ -97,26 +175,14 @@ Public Class Form1
                     txtPosition.Clear()
                     txtSalary.Clear()
                     txtDepartment.Clear()
+                    selectedId = -1
+
                 End Using
+
             End Using
-
         Catch ex As Exception
-
             MsgBox(ex.Message)
-
         End Try
-
-    End Sub
-
-    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
-        If e.RowIndex >= 0 Then
-            Dim selectRow As DataGridViewRow = DataGridView1.Rows(e.RowIndex)
-            txtId.Text = selectRow.Cells("id").Value.ToString()
-            txtName.Text = selectRow.Cells("name").Value.ToString()
-            txtPosition.Text = selectRow.Cells("position").Value.ToString()
-            txtSalary.Text = selectRow.Cells("salary").Value.ToString()
-            txtDepartment.Text = selectRow.Cells("department").Value.ToString()
-        End If
 
     End Sub
 
